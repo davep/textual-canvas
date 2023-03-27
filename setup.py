737d199b7f9d47ae6adaf835236fe5d5663a71f4
Dave Pearson <davep@davep.org>
2023-03-27 19:52:05 +0100

    :rocket: Add initial setup data

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup file for the textual-canvas library."""
+
+##############################################################################
+# Python imports.
+from setuptools import setup
+
+##############################################################################
+# Perform the setup.
+if __name__ == "__main__":
+    setup()
+
+### setup.py ends here
